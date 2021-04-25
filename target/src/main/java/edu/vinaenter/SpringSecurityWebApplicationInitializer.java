7f1813f338d7a75e40a15279c3426529e94a51bc
levanquoc <levanquocbkdn@gmail.com>
2021-04-25 03:36:07 +0000

    Deleted file

--- target/src/main/java/edu/vinaenter/SpringSecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package edu.vinaenter;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-	// do nothing
-
-}
